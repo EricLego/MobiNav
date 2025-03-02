60683e1ab36bee7c2c67483a33b3445878725e1c
devan137 <159643602+devan137@users.noreply.github.com>
2025-03-02 12:45:36 -0500

    Trafficsim update remote start
    Added python file to start simulation through Traci API.

---
+++ trafficsim/run_sumo.py
@@ -0,0 +1,11 @@
+import traci
+
+sumo_config_path = "/home/administrator/sumo/trafficsim/osm.sumocfg"  # Full path to config file
+
+traci.start(["sumo", "-c", sumo_config_path])
+
+for step in range(1000):  # Run for 1000 simulation steps
+    traci.simulationStep()
+
+traci.close()
+print("Simulation finished.")
