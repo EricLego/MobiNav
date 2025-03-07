b8683c7974e1417e0754fb25bc18a78a99a2c15c
devan137 <159643602+devan137@users.noreply.github.com>
2025-03-06 20:59:18 -0500

    Added GPS coordinates
    Added ability to identify edges and edge position based on GPS coordinates.

--- trafficsim/run_sumo_KSUMarietta.py
+++ trafficsim/run_sumo_KSUMarietta.py
@@ -1,53 +1,36 @@
 import traci
+import sumolib
 import random
 
-traci.start(["sumo", "-c", "KSUMarietta.sumocfg"])
+traci.start(["sumo-gui", "-c", "KSUMariettaConfig.sumocfg"])
+net = sumolib.net.readNet("KSUMarietta.net.xml.gz")
 
-all_edges = traci.edge.getIDList()
-pedestrian_edges = []
-
-# Find allowed vehicle classes for each edge
-for edge in all_edges:
-    lane_count = traci.edge.getLaneNumber(edge) #geta number of lanes in each edge
-    for i in range(lane_count):
-        lane_id = f"{edge}_{i}" #generates lane id
-        #Find edges that allow pedestrians
-        try:
-            allowed_vehicles = traci.lane.getAllowed(lane_id) #gets the allowed vehicle types for the edge (i.e., car, truck, pedestrian, etc.)
-            if "pedestrian" in allowed_vehicles: #if teh lane allows pedestrian, add the edge to pedestrian_edges and exit inner for loop
-                pedestrian_edges.append(edge)
-                break
-        except traci.TraCIException as e:
-            print(f"Error retrieving data for lane {lane_id}: {e}")
+# Find the nearest edge from coordinates
+cur_lat, cur_lon = 33.939520, -84.518931
+# Replace with current GPS coordinates
+dest_lat, dest_lon = 33.9365841870687, -84.52225348427882  # Replace with GPS coordinates pulled from database
+cur_edge, cur_edge_position, cur_lane_index = traci.simulation.convertRoad(cur_lon, cur_lat, isGeo=1)
+dest_edge, dest_edge_position, dest_lane_index = traci.simulation.convertRoad(dest_lon, dest_lat, isGeo=1)
+print(f"cur_edge: {cur_edge} and dest_edge: {dest_edge}")
 
 # Spawn number of pedestrians dynamically
-for i in range(1):
-    start_edge = "225485336#4" #specifies starting edge
-    end_edge = random.choice(pedestrian_edges)
-    while end_edge.startswith(":"):
-        end_edge = random.choice(pedestrian_edges)
-    print(f"Ending edge: {end_edge}")
+for i in range(100):
+    start_edge = cur_edge #specifies starting edge
+    end_edge = dest_edge #random.choice(pedestrian_edges)
     ped_id = f"ped_{i+1}" # Unique pedestrian ID
-
-
     route = traci.simulation.findRoute(start_edge, end_edge, vType="ped_pedestrian", depart=0, routingMode=1)
-    route_edges = [route.edges]
     route_edges = [edge for edge in route.edges if not edge.startswith(":")]
 
  # Add pedestrian to SUMO
-    traci.person.add(personID=ped_id, edgeID=start_edge, pos=0, typeID="ped_pedestrian", depart=traci.simulation.getTime())
+    traci.person.add(personID=ped_id, edgeID=start_edge, pos=cur_edge_position, typeID="ped_pedestrian", depart=random.randint(5,900))
 
 # Assign random walk path for random duration
-    traci.person.appendWalkingStage(personID=ped_id, edges=route_edges, arrivalPos=-1, duration=300)
-
-print(f"{ped_id} has end edge of: {end_edge} with a route of: {route_edges}")
-
+    traci.person.appendWalkingStage(personID=ped_id, edges=route_edges, arrivalPos=dest_edge_position, duration=300)
 
 # Run the simulation for the following number of steps
 for step in range(1000):
     traci.simulationStep()
     print(f"Simulation step {traci.simulation.getTime()}")
-    #print(f"ped_1 is located at: {traci.person.getPosition("ped_1")}")
 
 traci.close()
 print("Simulation done.")
